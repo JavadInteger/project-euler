39b8016eb61dabdc66d62f71a9d5c17d89a765d3
Integer <m1357.m.o.ng@gmail.com>
2025-06-26 01:27:30 +0330

    Create C Plus Plus File
    Create C Plus Plus File

---
+++ projecteuler1.cpp
@@ -0,0 +1,15 @@
+// C++
+#include <iostream>
+
+int main()
+{
+  int result = 0;
+  for (int i = 1; i < 1000; i++)
+  {
+    if (((i % 3) == 0) || ((i % 5) == 0)) 
+    {  
+      result += i;
+    }
+  }
+  std::cout << "The result is: " << result;
+}
